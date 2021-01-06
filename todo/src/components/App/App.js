fc00ec4e107e552bce2c08d106298b14d9dc8e88
Anya Vigolajnen <vigolajnen@gmail.com>
2021-01-06 23:42:03 +0300

    рефакторинг файла App.js

--- todo/src/components/App/App.js
+++ todo/src/components/App/App.js
@@ -4,30 +4,31 @@ import InputItem from '../InputItem/InputItem';
 import ItemList from '../ItemList/ItemList';
 import style from './App.module.css';
 
-const App = () => {
+class App extends React.Component {
+  render() {
+    const items = [
+      {
+        value: 'Задача № 1',
+        isDone: false
+      },
+      {
+        value: 'Задача № 2',
+        isDone: false
+      },
+      {
+        value: 'Задача № 3',
+        isDone: false
+      }
+    ];
 
-  const items = [
-    {
-      value: 'Задача № 1',
-      isDone: false
-    },
-    {
-      value: 'Задача № 2',
-      isDone: false
-    },
-    {
-      value: 'Задача № 3',
-      isDone: false
-    }
-  ];
-
-  return (<div className={style.wrapper}>
-    <h1 className={style.title}>Список задач</h1>
-    <InputItem />
-    <ItemList items={items} />
-    <Footer count={5} />
-  </div>
-  );
+    return (<div className={style.wrapper}>
+      <h1 className={style.title}>Список задач</h1>
+      <InputItem />
+      <ItemList items={items} />
+      <Footer count={5} />
+    </div>
+    );
+  }
 }
 
 export default App;
