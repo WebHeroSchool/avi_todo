66af949e25212b8464c2a3dadee076203532419f
Anya Vigolajnen <vigolajnen@gmail.com>
2021-01-06 13:10:55 +0300

    вывод элеметнов списка через map

--- todo/src/components/App/App.js
+++ todo/src/components/App/App.js
@@ -3,14 +3,27 @@ import Footer from '../Footer/Footer';
 import InputItem from '../InputItem/InputItem';
 import ItemList from '../ItemList/ItemList';
 
-const todoItem = 'Задача № ';
+const App = () => {
 
-const App = () => (<div>
-  <h1>Список</h1>
-  <InputItem />
-  <ItemList todoItem={todoItem} />
-  <Footer count={5} />
-</div>
-);
+  const items = [
+    {
+      value: 'Задача № 1'
+    },
+    {
+      value: 'Задача № 2'
+    },
+    {
+      value: 'Задача № 3'
+    }
+  ];
+
+  return (<div>
+    <h1>Список</h1>
+    <InputItem />
+    <ItemList items={items} />
+    <Footer count={5} />
+  </div>
+  );
+}
 
 export default App;
