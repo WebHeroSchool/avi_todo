95017d524eaf83e2a68ff3e7974120c835cd1960
Anya Vigolajnen <vigolajnen@gmail.com>
2021-01-06 23:52:16 +0300

    добавила state

--- todo/src/components/App/App.js
+++ todo/src/components/App/App.js
@@ -5,8 +5,8 @@ import ItemList from '../ItemList/ItemList';
 import style from './App.module.css';
 
 class App extends React.Component {
-  render() {
-    const items = [
+  state = {
+    items: [
       {
         value: 'Задача № 1',
         isDone: false
@@ -19,12 +19,15 @@ class App extends React.Component {
         value: 'Задача № 3',
         isDone: false
       }
-    ];
+    ]
+  };
+
+  render() {
 
     return (<div className={style.wrapper}>
       <h1 className={style.title}>Список задач</h1>
       <InputItem />
-      <ItemList items={items} />
+      <ItemList items={this.state.items} />
       <Footer count={5} />
     </div>
     );
