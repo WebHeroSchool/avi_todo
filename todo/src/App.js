c5900231bf6c50855f3ad594989bbd26783bf6bf
Anya Vigolajnen <vigolajnen@gmail.com>
2021-01-06 03:06:36 +0300

    mod4

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,21 +1,7 @@
-import logo from './logo.svg';
-import {count, length} from './number.js'
-import './App.css';
+import React from 'react';
 
+const element = (<div>jjjj
+ <a>go it</a>
+</div>);
 
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-
-        <p style={{ color: "yellow", fontSize: 10 + 20 }}>Lorem, ipsum dolor.</p>
-        <p>Итого: {count * length}</p>
-        
-
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export { element };
