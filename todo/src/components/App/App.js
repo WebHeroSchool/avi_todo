55ebb96599a70a5f2e4594b92a458bfd4e02548a
Anya Vigolajnen <vigolajnen@gmail.com>
2021-01-06 11:37:25 +0300

    Структура проекта todoList

---
+++ todo/src/components/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Footer from '../Footer/Footer';
+import InputItem from '../InputItem/InputItem';
+import ItemList from '../ItemList/ItemList';
+
+const App = () => (<div>
+  <h1>Список</h1>
+  <InputItem />
+  <ItemList />
+  <Footer />
+</div>
+);
+
+export default App;
